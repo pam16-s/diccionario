48d01cdb5edc64c81901fa5611b04fbca919a329
pam16-s <anbaupamela2021@gmail.com>
2025-02-25 21:06:39 -0500

    Create diccionario.py
    diccionario de memes.

---
+++ diccionario.py
@@ -0,0 +1,17 @@
+None
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL":"una respuesta a una broma",
+            "SHEESH":" ligera desaprobación",
+            "CREEPY": "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado"
+             
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    print("vuelve a intentarlo")
+    # ¿Qué hacer si no se encuentra la palabra?
